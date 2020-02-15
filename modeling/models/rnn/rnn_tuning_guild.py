0b00fe5ecf5b9e78809b8bbb438ec85570f449fa
Mateusz Jukiewicz <jukiewiczm@gmail.com>
2020-02-15 20:46:40 +0100

    Issue #2 allowed preparing submission from trained model, changed tuning experiment

--- modeling/models/rnn/rnn_tuning_guild.py
+++ modeling/models/rnn/rnn_tuning_guild.py
@@ -6,14 +6,15 @@ from categorical_variables_embeddings.generate_dense_datasets import StarspaceDe
 from modeling.model_validation import StandardValidation
 from modeling.models.rnn.torch_rnn_net import RNNNet
 from modeling.utils import read_parquet
+from glob import glob
 
 target_col = "item_cnt_month"
 
-prepare_submission = 0  # Change to 1 if you want to re-run guild job with tuned parameters.
+run_type = "evaluate"
 test_reference = None
 submission_name = "submission.csv.gz"
 
-if prepare_submission:
+if run_type != "evaluate":
     train_set = read_parquet("data/processed_full/train.parquet")
     test_set = read_parquet("data/processed_full/test.parquet")
     test_reference = test_set[['ID']].astype("int32")
@@ -64,12 +65,13 @@ if embedding_type == "wiki":
     embedding_size_final = 300
     average_dense_sets_final = 1  # wiki embeddings are too big to concatenate them
 
-produce_dense_dataset(generator, "data/items.csv", 'item_id', 'item_name',
-                      "guild_item_embeddings.parquet")
-produce_dense_dataset(generator, "data/item_categories.csv", 'item_category_id', 'item_category_name',
-                      "guild_item_category_embeddings.parquet")
-produce_dense_dataset(generator, "data/shops.csv", 'shop_id', 'shop_name',
-                      "guild_shop_embeddings.parquet")
+if run_type != "load_predict":
+    produce_dense_dataset(generator, "data/items.csv", 'item_id', 'item_name',
+                          "guild_item_embeddings.parquet")
+    produce_dense_dataset(generator, "data/item_categories.csv", 'item_category_id', 'item_category_name',
+                          "guild_item_category_embeddings.parquet")
+    produce_dense_dataset(generator, "data/shops.csv", 'shop_id', 'shop_name',
+                          "guild_shop_embeddings.parquet")
 
 ## RNN modules
 rnn_modules = {"gru": torch.nn.GRU, "lstm": torch.nn.LSTM, "rnn": torch.nn.RNN}
@@ -93,13 +95,21 @@ model = RNNNet(
     save_temporary_models=False, plot_training_history=False
 )
 
-if prepare_submission:
+if run_type != "evaluate":
     # Train the model, then predict
     y_train = train_set[target_col]
     train_set.drop(target_col, axis=1, inplace=True)
     # Ensure proper column order just in case
     test_set = test_set[train_set.columns]
-    model.fit(train_set, y_train)
+
+    if run_type == "load_predict":
+        # If you use this option and don't have the actual model trained, this will fail!
+        model.num_epochs = 0
+        model.fit(train_set, y_train)
+        model.load_state_dict(torch.load("model.pt"))
+    else:
+        model.fit(train_set, y_train)
+
     predictions = model.transform(test_set).clip(0, 20)
     test_reference['item_cnt_month'] = predictions
     test_reference.sort_values('ID').to_csv(f"submissions/{submission_name}", index=False, header=True, compression='gzip')
@@ -108,6 +118,6 @@ else:
     validation = StandardValidation(target_col, model)
     validation_result = validation.run(train_set, test_set, inner_train_validation=False, copy=False)
 
-    torch.save(validation.model.state_dict(), f"model_{validation_result}.pt")
+    torch.save(validation.model.state_dict(), "model.pt")
 
     print(f"loss: {validation_result}")
